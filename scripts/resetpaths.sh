8d2d514ca312e200e7a54346eb11fc66a5d20790
Gary Hockin <gary@hock.in>
2022-03-07 14:35:32 +0000

    Added extra step to sort paths on runonce

---
+++ scripts/resetpaths.sh
@@ -0,0 +1,1 @@
+sed -ie "s|\/Users\/Demo1|${HOME}|g" scenes/Booth_Demo.json
--- scripts/runonce.sh
+++ scripts/runonce.sh
@@ -2,3 +2,6 @@ echo "
 [filter \"change-home-path\"]
   smudge = scripts/smudge.sh
   clean = scripts/clean.sh" >> .git/config
+
+
+scripts/resetpaths.sh
